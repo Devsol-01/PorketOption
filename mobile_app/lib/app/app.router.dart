8c91206e4bda39970016d7eb33ffa086ff59cfb6
uche solomon <solomonu928@gmail.com>
2025-07-18 16:47:07 +0100

    feat: Add verification view and update routing in app.router.dart

--- mobile_app/lib/app/app.router.dart
+++ mobile_app/lib/app/app.router.dart
@@ -5,7 +5,7 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:flutter/material.dart' as _i14;
+import 'package:flutter/material.dart' as _i15;
 import 'package:flutter/material.dart';
 import 'package:mobile_app/ui/views/bottom_nav/bottom_nav_view.dart' as _i4;
 import 'package:mobile_app/ui/views/dashboard/dashboard_view.dart' as _i3;
@@ -19,8 +19,10 @@ import 'package:mobile_app/ui/views/onboarding/onboarding_view.dart' as _i12;
 import 'package:mobile_app/ui/views/profile/profile_view.dart' as _i6;
 import 'package:mobile_app/ui/views/saving/saving_view.dart' as _i7;
 import 'package:mobile_app/ui/views/startup/startup_view.dart' as _i2;
+import 'package:mobile_app/ui/views/verification/verification_view.dart'
+    as _i14;
 import 'package:stacked/stacked.dart' as _i1;
-import 'package:stacked_services/stacked_services.dart' as _i15;
+import 'package:stacked_services/stacked_services.dart' as _i16;
 
 class Routes {
   static const startupView = '/startup-view';
@@ -47,6 +49,8 @@ class Routes {
 
   static const emailView = '/email-view';
 
+  static const verificationView = '/verification-view';
+
   static const all = <String>{
     startupView,
     dashboardView,
@@ -60,6 +64,7 @@ class Routes {
     groupSaveView,
     onboardingView,
     emailView,
+    verificationView,
   };
 }
 
@@ -113,78 +118,88 @@ class StackedRouter extends _i1.RouterBase {
       Routes.emailView,
       page: _i13.EmailView,
     ),
+    _i1.RouteDef(
+      Routes.verificationView,
+      page: _i14.VerificationView,
+    ),
   ];
 
   final _pagesMap = <Type, _i1.StackedRouteFactory>{
     _i2.StartupView: (data) {
-      return _i14.MaterialPageRoute<dynamic>(
+      return _i15.MaterialPageRoute<dynamic>(
         builder: (context) => const _i2.StartupView(),
         settings: data,
       );
     },
     _i3.DashboardView: (data) {
-      return _i14.MaterialPageRoute<dynamic>(
+      return _i15.MaterialPageRoute<dynamic>(
         builder: (context) => const _i3.DashboardView(),
         settings: data,
       );
     },
     _i4.BottomNavView: (data) {
-      return _i14.MaterialPageRoute<dynamic>(
+      return _i15.MaterialPageRoute<dynamic>(
         builder: (context) => const _i4.BottomNavView(),
         settings: data,
       );
     },
     _i5.InvestmentView: (data) {
-      return _i14.MaterialPageRoute<dynamic>(
+      return _i15.MaterialPageRoute<dynamic>(
         builder: (context) => const _i5.InvestmentView(),
         settings: data,
       );
     },
     _i6.ProfileView: (data) {
-      return _i14.MaterialPageRoute<dynamic>(
+      return _i15.MaterialPageRoute<dynamic>(
         builder: (context) => const _i6.ProfileView(),
         settings: data,
       );
     },
     _i7.SavingView: (data) {
-      return _i14.MaterialPageRoute<dynamic>(
+      return _i15.MaterialPageRoute<dynamic>(
         builder: (context) => const _i7.SavingView(),
         settings: data,
       );
     },
     _i8.GoalSaveView: (data) {
-      return _i14.MaterialPageRoute<dynamic>(
+      return _i15.MaterialPageRoute<dynamic>(
         builder: (context) => const _i8.GoalSaveView(),
         settings: data,
       );
     },
     _i9.FlexiSaveView: (data) {
-      return _i14.MaterialPageRoute<dynamic>(
+      return _i15.MaterialPageRoute<dynamic>(
         builder: (context) => const _i9.FlexiSaveView(),
         settings: data,
       );
     },
     _i10.LockSaveView: (data) {
-      return _i14.MaterialPageRoute<dynamic>(
+      return _i15.MaterialPageRoute<dynamic>(
         builder: (context) => const _i10.LockSaveView(),
         settings: data,
       );
     },
     _i11.GroupSaveView: (data) {
-      return _i14.MaterialPageRoute<dynamic>(
+      return _i15.MaterialPageRoute<dynamic>(
         builder: (context) => const _i11.GroupSaveView(),
         settings: data,
       );
     },
     _i12.OnboardingView: (data) {
-      return _i14.MaterialPageRoute<dynamic>(
+      return _i15.MaterialPageRoute<dynamic>(
         builder: (context) => const _i12.OnboardingView(),
         settings: data,
       );
     },
     _i13.EmailView: (data) {
-      return _i14.MaterialPageRoute<dynamic>(
+      return _i15.MaterialPageRoute<dynamic>(
         builder: (context) => const _i13.EmailView(),
+        settings: data,
+      );
+    },
+    _i14.VerificationView: (data) {
+      return _i15.MaterialPageRoute<dynamic>(
+        builder: (context) => const _i14.VerificationView(),
         settings: data,
       );
     },
@@ -197,7 +212,7 @@ class StackedRouter extends _i1.RouterBase {
   Map<Type, _i1.StackedRouteFactory> get pagesMap => _pagesMap;
 }
 
-extension NavigatorStateExtension on _i15.NavigationService {
+extension NavigatorStateExtension on _i16.NavigationService {
   Future<dynamic> navigateToStartupView([
     int? routerId,
     bool preventDuplicates = true,
@@ -366,6 +381,20 @@ extension NavigatorStateExtension on _i15.NavigationService {
         transition: transition);
   }
 
+  Future<dynamic> navigateToVerificationView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return navigateTo<dynamic>(Routes.verificationView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
   Future<dynamic> replaceWithStartupView([
     int? routerId,
     bool preventDuplicates = true,
@@ -528,6 +557,20 @@ extension NavigatorStateExtension on _i15.NavigationService {
         transition,
   ]) async {
     return replaceWith<dynamic>(Routes.emailView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithVerificationView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return replaceWith<dynamic>(Routes.verificationView,
         id: routerId,
         preventDuplicates: preventDuplicates,
         parameters: parameters,
